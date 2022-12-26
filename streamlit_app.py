a4de4f420a9c9d62020c80ef04ac6bf8fb956c95
Wendy T <105395847+wendyhtam@users.noreply.github.com>
2022-12-26 18:42:15 -0500

    Update streamlit_app.py
    Added the smoothie header
    Added the fruit list dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,6 @@
 import streamlit
 import pandas
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
-
 streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header('Breakfast Favorites')
@@ -12,3 +9,7 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avovado Toast')
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
+streamlit.header("Build Your Own Fruit Smoothie")
