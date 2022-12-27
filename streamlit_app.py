2eb2cc65367b1a95e72d6f0964dcaff8dc598a11
Wendy T <105395847+wendyhtam@users.noreply.github.com>
2022-12-27 09:13:35 -0500

    Update streamlit_app.py
    Added emoji to the smoothie header

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avovado Toast')
 
-streamlit.header("Build Your Own Fruit Smoothie")
+streamlit.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 
