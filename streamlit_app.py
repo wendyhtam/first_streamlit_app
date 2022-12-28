8b3ffffe6d9207659fead3bb41c30962e499c747
Wendy T <105395847+wendyhtam@users.noreply.github.com>
2022-12-28 12:06:39 -0500

    Update streamlit_app.py
    Added the insert new fruit section

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,3 +43,5 @@ streamlit.dataframe(my_data_rows)
 
 # Add another text box to add more fruit
 add_my_fruit = streamlit.text_input("What fruit would you like to add?")
+streamlit.write('Thanks for adding ', add_my_fruit)
+my_cur.execute("insert into fruit_load_list values ('from streamlit')");
