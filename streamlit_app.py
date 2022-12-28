b64f093356386eda4aca08ed62acb33a960a3241
Wendy T <105395847+wendyhtam@users.noreply.github.com>
2022-12-28 12:03:48 -0500

    Update streamlit_app.py
    Added second text input box to get new fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,3 +40,6 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+# Add another text box to add more fruit
+add_my_fruit = streamlit.text_input("What fruit would you like to add?")
