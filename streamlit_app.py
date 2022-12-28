07cb40a2e3cd6f1a856e9b5c0e3a4b813d0f5af4
Wendy T <105395847+wendyhtam@users.noreply.github.com>
2022-12-28 12:26:31 -0500

    Update streamlit_app.py
    Added to stop() function to troubleshoot

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,6 +57,8 @@ if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
+    
+streamlit.stop()
 
 # Add another text box to add more fruit
 add_my_fruit = streamlit.text_input("What fruit would you like to add?")
